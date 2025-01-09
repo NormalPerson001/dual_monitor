8ffaf4f4d3ec5180be63f8d3a9e2c2b0047b0f44
Normal Person <131663287+NormalPerson001@users.noreply.github.com>
2025-01-09 07:54:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo apt install linux-headers-$(uname -r) nvidia-driver nvidia-xconfig && sudo update-initramfs -u 
+sudo apt install linux-headers-$(uname -r) -y && sudo apt install nvidia-drivers nvidia-xconfig -y && sudo update-initramfs -u 
 mv display_vidia.sh ~/.local/bin/
